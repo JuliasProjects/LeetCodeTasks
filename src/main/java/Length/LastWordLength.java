cd1ede081b7816652d645822e880624bf99f7bc2
JuliasProjects <julia.bouza_bozhok@mail.ru>
2022-02-24 11:35:58 +0300

    done

---
+++ src/main/java/Length/LastWordLength.java
@@ -0,0 +1,14 @@
+package Length;
+
+public class LastWordLength {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        s = s.trim();
+        int start = s.length() - 1;
+        while (start >= 0 && s.charAt(start) != ' ') {
+            length++;
+            start--;
+        }
+        return length;
+    }
+}
